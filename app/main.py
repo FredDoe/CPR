a215604746ac192ff93d342bf43ea0f32d644151
Godfred Doe <godfredoe9@gmail.com>
2023-09-02 11:59:27 +0000

    formatting with black enabled

--- app/main.py
+++ app/main.py
@@ -6,18 +6,29 @@ import multiprocessing as mp
 
 import app.configuration
 import app.utilities
-from app import emitter, logger, definitions, values, builder, repair, \
-    configuration, reader, parallel, extractor,  oracle
+from app import (
+    emitter,
+    logger,
+    definitions,
+    values,
+    builder,
+    repair,
+    configuration,
+    reader,
+    parallel,
+    extractor,
+    oracle,
+)
 from app.concolic import run_concrete_execution
 
 start_time = 0
 time_info = {
-    definitions.KEY_DURATION_INITIALIZATION: '0',
-    definitions.KEY_DURATION_BUILD: '0',
-    definitions.KEY_DURATION_BOOTSTRAP: '0',
-    definitions.KEY_DURATION_REPAIR: '0',
-    definitions.KEY_DURATION_TOTAL: '0'
-    }
+    definitions.KEY_DURATION_INITIALIZATION: "0",
+    definitions.KEY_DURATION_BUILD: "0",
+    definitions.KEY_DURATION_BOOTSTRAP: "0",
+    definitions.KEY_DURATION_REPAIR: "0",
+    definitions.KEY_DURATION_TOTAL: "0",
+}
 
 stop_event = mp.Event()
 
@@ -74,13 +85,16 @@ def initialize():
     test_case_id = 0
     count_seeds = len(values.LIST_SEED_INPUT)
     count_inputs = len(test_input_list)
-    for argument_list in test_input_list[:count_inputs - count_seeds]:
+    for argument_list in test_input_list[: count_inputs - count_seeds]:
         print_argument_list = app.configuration.extract_input_arg_list(argument_list)
         generalized_arg_list = []
         seed_file = None
         test_case_id = test_case_id + 1
         for arg in print_argument_list:
-            if arg in (list(values.LIST_SEED_FILES.values()) + list(values.LIST_TEST_FILES.values())):
+            if arg in (
+                list(values.LIST_SEED_FILES.values())
+                + list(values.LIST_TEST_FILES.values())
+            ):
                 generalized_arg_list.append("$POC")
                 seed_file = arg
             else:
@@ -102,7 +116,9 @@ def initialize():
         if not os.path.isfile(program_path + ".bc"):
             app.utilities.error_exit("Unable to generate bytecode for " + program_path)
 
-        exit_code = run_concrete_execution(program_path + ".bc", argument_list, True, klee_out_dir)
+        exit_code = run_concrete_execution(
+            program_path + ".bc", argument_list, True, klee_out_dir
+        )
         assert exit_code == 0
         # set location of bug/crash
         values.IS_CRASH = False
@@ -111,7 +127,9 @@ def initialize():
         #     values.USEFUL_SEED_ID_LIST.append(test_case_id)
         if latest_crash_loc:
             values.IS_CRASH = True
-            emitter.success("\t\t\t[info] identified a crash location: " + str(latest_crash_loc))
+            emitter.success(
+                "\t\t\t[info] identified a crash location: " + str(latest_crash_loc)
+            )
             if latest_crash_loc not in values.CONF_LOC_LIST_CRASH:
                 values.CONF_LOC_LIST_CRASH.append(latest_crash_loc)
 
@@ -124,7 +142,7 @@ def run(arg_list):
 
     time_check = time.time()
     bootstrap(arg_list)
-    duration = format((time.time() - time_check) / 60, '.3f')
+    duration = format((time.time() - time_check) / 60, ".3f")
     time_info[definitions.KEY_DURATION_BOOTSTRAP] = str(duration)
 
     time_check = time.time()
@@ -133,32 +151,35 @@ def run(arg_list):
         if values.CONF_PATH_PROGRAM:
             assert os.path.isfile(values.CONF_PATH_PROGRAM)
             assert os.path.getsize(values.CONF_PATH_PROGRAM) > 0
-    duration = format((time.time() - time_check) / 60, '.3f')
+    duration = format((time.time() - time_check) / 60, ".3f")
     time_info[definitions.KEY_DURATION_BUILD] = str(duration)
 
     time_check = time.time()
     if not values.CONF_SKIP_TEST:
         initialize()
-    duration = format((time.time() - time_check) / 60, '.3f')
+    duration = format((time.time() - time_check) / 60, ".3f")
     time_info[definitions.KEY_DURATION_INITIALIZATION] = str(duration)
 
     if values.CONF_ONLY_TEST:
         return
     time_check = time.time()
     repair.run(values.CONF_PATH_PROJECT, values.CONF_PATH_PROGRAM)
-    duration = format(((time.time() - time_check) / 60 - float(values.TIME_TO_GENERATE)), '.3f')
+    duration = format(
+        ((time.time() - time_check) / 60 - float(values.TIME_TO_GENERATE)), ".3f"
+    )
     time_info[definitions.KEY_DURATION_REPAIR] = str(duration)
 
 
 def main():
     import sys
+
     is_error = False
     signal.signal(signal.SIGALRM, timeout_handler)
     signal.signal(signal.SIGTERM, shutdown)
     try:
         run(sys.argv[1:])
     except SystemExit as e:
-        total_duration = format((time.time() - start_time) / 60, '.3f')
+        total_duration = format((time.time() - start_time) / 60, ".3f")
         time_info[definitions.KEY_DURATION_TOTAL] = str(total_duration)
         emitter.end(time_info, is_error)
         logger.end(time_info, is_error)
@@ -167,7 +188,7 @@ def main():
         parallel.pool.join()
         # os.system("ps -aux | grep 'cpr' | awk '{print $2}' | xargs kill -9")
     except KeyboardInterrupt as e:
-        total_duration = format((time.time() - start_time) / 60, '.3f')
+        total_duration = format((time.time() - start_time) / 60, ".3f")
         time_info[definitions.KEY_DURATION_TOTAL] = str(total_duration)
         emitter.end(time_info, is_error)
         logger.end(time_info, is_error)
@@ -180,7 +201,7 @@ def main():
     finally:
         # Final running time and exit message
         # os.system("ps -aux | grep 'python' | awk '{print $2}' | xargs kill -9")
-        total_duration = format((time.time() - start_time) / 60, '.3f')
+        total_duration = format((time.time() - start_time) / 60, ".3f")
         time_info[definitions.KEY_DURATION_TOTAL] = str(total_duration)
         emitter.end(time_info, is_error)
         logger.end(time_info, is_error)
